b4e1e5117975869c744c461256486306e308a6fa
Tirth Sompura <36097066+tirthsompura@users.noreply.github.com>
2024-02-22 13:37:58 +0530

    Delete app/build directory

--- app/build/generated/data_binding_trigger/debug/com/example/googlemapdirection/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.googlemapdirection;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_googlemapdirection_App.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.googlemapdirection.App"
-)
-public final class _com_example_googlemapdirection_App {
-}
