73f80c708e4a1a25d89dac438a41664341508b10
Tirth Sompura <36097066+tirthsompura@users.noreply.github.com>
2024-02-23 12:03:12 +0530

    Update EmployeeScreen.kt

--- app/src/main/java/com/example/googlemapdirection/screens/home/EmployeeScreen.kt
+++ app/src/main/java/com/example/googlemapdirection/screens/home/EmployeeScreen.kt
@@ -1,5 +1,7 @@
 package com.example.googlemapdirection.screens.home
 
+import android.location.Address
+import android.location.Geocoder
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,12 +15,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.googlemapdirection.screens.LocationPermissionScreen
 import com.example.googlemapdirection.screens.MapScreen
 import com.example.googlemapdirection.screens.common.ConfirmationDialogBottomSheet
-import com.example.googlemapdirection.R
 import com.example.googlemapdirection.utils.FirebaseKeyConstants
 import com.example.googlemapdirection.utils.FirebaseKeyConstants.AcceptReq
 import com.example.googlemapdirection.utils.FirebaseKeyConstants.DefaultReq
@@ -27,10 +27,14 @@ import com.example.googlemapdirection.utils.FirebaseKeyConstants.EMP_LAT
 import com.example.googlemapdirection.utils.FirebaseKeyConstants.EMP_LONG
 import com.example.googlemapdirection.utils.FirebaseKeyConstants.EMP_NAME
 import com.example.googlemapdirection.utils.FirebaseKeyConstants.Request
+import com.example.googlemapdirection.utils.FirebaseKeyConstants.USER_LET
+import com.example.googlemapdirection.utils.FirebaseKeyConstants.USER_LONG
+import com.example.googlemapdirection.utils.FirebaseKeyConstants.USER_NAME
 import com.example.googlemapdirection.utils.SessionManagerClass
 import com.example.googlemapdirection.utils.checkForPermission
 import com.example.googlemapdirection.utils.showToast
 import com.google.android.gms.maps.model.LatLng
+import java.util.Locale
 
 @Composable
 fun EmployeeScreen(
@@ -61,11 +65,47 @@ fun EmployeeScreen(
         }
     }
 
+    val userName = remember {
+        mutableStateOf("")
+    }
+    val userAddress = remember {
+        mutableStateOf("")
+    }
+    val latLng = remember {
+        mutableStateOf(LatLng(0.0, 0.0))
+    }
+
     if (openBottomSheetDialog.value) {
+
+        viewModel.data.value.forEach { (key, value) ->
+            val userNameKey = USER_NAME
+            val userNameData = viewModel.data.value[userNameKey]
+
+            userName.value = userNameData.toString()
+
+            val userLongKey = USER_LONG
+            val userLongData = viewModel.data.value[userLongKey]
+
+            val userLetKey = USER_LET
+            val userLetData = viewModel.data.value[userLetKey]
+            latLng.value =
+                LatLng(userLetData.toString().toDouble(), userLongData.toString().toDouble())
+        }
+
+        if (latLng.value.latitude.toString().toDouble() != 0.0) {
+            val geocoder = Geocoder(context, Locale.getDefault())
+            val addresses: List<Address>? =
+                geocoder.getFromLocation(latLng.value.latitude, latLng.value.longitude, 1)
+            if (addresses?.isNotEmpty() == true) {
+                val address = addresses[0].getAddressLine(0)
+                userAddress.value = address.toString()
+            }
+        }
+
         ConfirmationDialogBottomSheet(
-            title = "Accept the order",
-            label = "Do you want to go to ride?",
-            icon = painterResource(id = R.drawable.ic_blue_bell),
+            userName = userName,
+            address = userAddress.value,
+            title = "New Ride Request",
             onDismiss = {
                 viewModel.updateDocumentEmployeeDetails(
                     "JFhEqMwqRZm9RGX9xuJc",
